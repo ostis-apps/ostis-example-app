286017b138aed67940e8e0edcbdd2c91e02c4f2e
Valentin Vityaz <valik.vityaz@gmail.com>
2020-09-23 13:44:07 +0300

    style(install_ostis): fix indentation

--- scripts/install_ostis.sh
+++ scripts/install_ostis.sh
@@ -2,27 +2,27 @@
 
 prepare_platform()
 {
-		cd scripts
-		sudo apt-get update
-		./prepare.sh
-		cd ..
+	cd scripts
+	sudo apt-get update
+	./prepare.sh
+	cd ..
 }
 
 include_kb()
 {
-  rm ./ims.ostis.kb/ui/ui_start_sc_element.scs
-  rm -rf ./kb/menu
-  echo "../kb" >> ./repo.path
-  echo "../problem-solver" >> ./repo.path
-  cd scripts
-  ./build_kb.sh
-  cd ..
+	rm ./ims.ostis.kb/ui/ui_start_sc_element.scs
+	rm -rf ./kb/menu
+	echo "../kb" >> ./repo.path
+	echo "../problem-solver" >> ./repo.path
+	cd scripts
+	./build_kb.sh
+	cd ..
 }
 
 include_kpm()
 {
-  cd sc-machine
-  echo 'add_subdirectory(${SC_MACHINE_ROOT}/../../problem-solver/cxx ${SC_MACHINE_ROOT}/build/problem-solver)' >> ./CMakeLists.txt
+	cd sc-machine
+	echo 'add_subdirectory(${SC_MACHINE_ROOT}/../../problem-solver/cxx ${SC_MACHINE_ROOT}/build/problem-solver)' >> ./CMakeLists.txt
 	cd ./scripts
 	./make_all.sh
 	cd ../..
@@ -30,18 +30,18 @@ include_kpm()
 
 cd ..
 if [ -d "ostis" ]; 
-	then
-		echo -en "Update OSTIS platform\n"
-		cd ostis
-		git pull
-		prepare_platform
-	else
-		echo -en "Install OSTIS platform\n"
-		git clone https://github.com/ShunkevichDV/ostis.git
-		cd ostis
-		git checkout 0.5.0
-		prepare_platform
-		include_kb
-		include_kpm
+then
+    echo -en "Update OSTIS platform\n"
+    cd ostis
+    git pull
+    prepare_platform
+else
+    echo -en "Install OSTIS platform\n"
+    git clone https://github.com/ShunkevichDV/ostis.git
+    cd ostis
+    git checkout 0.5.0
+    prepare_platform
+    include_kb
+    include_kpm
 fi
 

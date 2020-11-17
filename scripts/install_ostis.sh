af4c51058443d2bc557ac4d9febf6f8bdf34bd03
KovalM <michail.kovalev217@gmail.com>
2020-11-17 22:39:45 +0300

    build(common): Optimize install custom python modules in sc-machine

--- scripts/install_ostis.sh
+++ scripts/install_ostis.sh
@@ -3,6 +3,7 @@
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
 PLATFORM_PATH="${APP_ROOT_PATH}/ostis-web-platform"
 WORKING_PATH=$(pwd)
+PYTHON_PATH="${APP_ROOT_PATH}"/problem-solver/py/services
 PLATFORM_REPO="https://github.com/ostis-dev/ostis-web-platform.git"
 
 prepare_platform()
@@ -38,13 +39,13 @@ include_kpm()
 	sed -i '\|build/problem-solver/py|d' ./CMakeLists.txt
 	cat "${APP_ROOT_PATH}"/scripts/sc_machine_cmake_file_ext.txt >> ./CMakeLists.txt
 	
-	cd config
-	sed -i '/python/d' ./config.ini.in
-	sed -i '/modules_path/d' ./config.ini.in 
-	echo -e '[python]' >> ./config.ini.in
-	echo 'modules_path = ${SC_MACHINE_ROOT}/sc-kpm/sc-python/services;${SC_MACHINE_ROOT}/../../problem-solver/py/services' >> ./config.ini.in
+	if ! grep -q "${PYTHON_PATH}" "${PLATFORM_PATH}/sc-machine/config/config.ini.in";
+	then
+		PYTHON_PATH_ESCAPED="$(echo "${PYTHON_PATH}" | sed -e 's/[/]/\\&/g')"
+		sed -i "/modules_path/ s/$/;${PYTHON_PATH_ESCAPED}/" "${PLATFORM_PATH}/sc-machine/config/config.ini.in"
+	fi
 
-	cd ../scripts
+	cd scripts
 	./make_all.sh
 	cat "${PLATFORM_PATH}"/bin/config.ini >> "${PLATFORM_PATH}"/config/sc-web.ini
 	cd "${PLATFORM_PATH}"

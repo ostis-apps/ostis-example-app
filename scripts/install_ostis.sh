31a2d3bfc3c1e4e7bbd01d1f0c68e8349944e048
SHtress <stas.butrin1331@gmail.com>
2022-09-05 20:56:01 +0300

    fix: fix paths in install_ostis

--- scripts/install_ostis.sh
+++ scripts/install_ostis.sh
@@ -3,35 +3,30 @@
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
 PLATFORM_PATH="${APP_ROOT_PATH}/ostis-web-platform"
 WORKING_PATH=$(pwd)
-PYTHON_PATH="${APP_ROOT_PATH}"/problem-solver/py/services
 PLATFORM_REPO="https://github.com/ostis-ai/ostis-web-platform.git"
 
 prepare_platform()
 {
 	cd "${PLATFORM_PATH}"/scripts
 	./prepare.sh
-	cd "${PLATFORM_PATH}"
 }
 
 prepare_platform_without_build()
 {
 	cd "${PLATFORM_PATH}"/scripts
 	./prepare.sh no_build_kb no_build_sc_machine
-	cd "${PLATFORM_PATH}"
 }
 
 include_kb()
 {
-	cd "${PLATFORM_PATH}"/scripts
+	cd "${APP_ROOT_PATH}"/scripts
 	./build_kb.sh
-	cd "${PLATFORM_PATH}"
 }
 
 include_problem_solver()
 {
 	cd "${APP_ROOT_PATH}"/scripts
 	./build_problem_solver.sh
-	cd "${PLATFORM_PATH}"
 }
 
 cd "${APP_ROOT_PATH}"
@@ -40,14 +35,12 @@ if [ -d "${PLATFORM_PATH}" ];
 		echo -en "Update OSTIS platform\n"
 		cd "${PLATFORM_PATH}"
 		git pull
-		"${APP_ROOT_PATH}"/scripts/clone_subsystems.sh
 		prepare_platform
 	else
 		echo -en "Install OSTIS platform\n"
 		git clone ${PLATFORM_REPO}
 		cd "${PLATFORM_PATH}"
 		git checkout prerelease/0.7.0
-		"${APP_ROOT_PATH}"/scripts/clone_subsystems.sh
 		prepare_platform_without_build
 		include_problem_solver
 		include_kb

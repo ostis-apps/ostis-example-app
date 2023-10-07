045b5aa1966a22234f5ce21fa9f54d4969ff2276
NikiforovSergei <nikiforov.sergei.al@gmail.com>
2023-10-07 18:39:53 +0300

    fix: set poject cmake file as root

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
 
 export APP_ROOT_PATH="${ROOT_PATH}"
+export ROOT_CMAKE_PATH="${APP_ROOT_PATH}"
 export CONFIG_PATH="${APP_ROOT_PATH}/ostis-example-app.ini"
 export REPO_PATH="${APP_ROOT_PATH}/repo.path"
 

d27c275388f0f734b6ca989e412c2c8c9fca95f3
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-02-01 00:03:40 +0300

    scripts: set 0.9.0 submodules

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -9,18 +9,18 @@ export CONFIG_PATH="${APP_ROOT_PATH}/ostis-example-app.ini"
 export REPO_PATH="${APP_ROOT_PATH}/repo.path"
 
 export PLATFORM_REPO="https://github.com/ostis-ai/ostis-web-platform.git"
-export PLATFORM_BRANCH="0.8.0-Fusion"
+export PLATFORM_BRANCH="0.9.0-Unlock"
 export PLATFORM_COMMIT=""
 export PLATFORM_PATH="${APP_ROOT_PATH}/ostis-web-platform"
 
 export SC_MACHINE_REPO="https://github.com/ostis-ai/sc-machine.git"
-export SC_MACHINE_BRANCH="0.8.0-Fusion"
+export SC_MACHINE_BRANCH="0.9.0-Unlock"
 export SC_MACHINE_COMMIT=""
 export SC_MACHINE_PATH="${PLATFORM_PATH}/sc-machine"
 export ROOT_CMAKE_PATH="${APP_ROOT_PATH}"
 
 export SC_WEB_REPO="https://github.com/ostis-ai/sc-web.git"
-export SC_WEB_BRANCH="0.8.0-Fusion"
+export SC_WEB_BRANCH="0.8.1-Unlock"
 export SC_WEB_COMMIT=""
 export SC_WEB_PATH="${PLATFORM_PATH}/sc-web"
 

1212eda12930d2738ad5a019d869d605165b3bcb
NikiforovSergei <nikiforov.sergei.al@gmail.com>
2025-01-10 17:42:35 +0300

    feat: add incorrect connector templates processing

--- problem-solver/cxx/exampleModule/utils/TemplateUtils.cpp
+++ problem-solver/cxx/exampleModule/utils/TemplateUtils.cpp
@@ -20,10 +20,16 @@ void TemplateUtils::getConnectorTemplateKeyElements(
 {
   elements.connectorStartVariable =
       IteratorUtils::getAnyByOutRelation(&context, connectorTemplateAddr, Keynodes::GetRrelIndex(1));
+  if(!elements.connectorStartVariable.IsValid())
+    SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "Incorrect connector template - connector start variable is not found.");
   elements.connectorEndVariable =
       IteratorUtils::getAnyByOutRelation(&context, connectorTemplateAddr, Keynodes::GetRrelIndex(2));
+  if(!elements.connectorEndVariable.IsValid())
+    SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "Incorrect connector template - connector end variable is not found.");
   elements.connectorVariable =
       IteratorUtils::getAnyByOutRelation(&context, connectorTemplateAddr, Keynodes::GetRrelIndex(3));
+  if(!elements.connectorVariable.IsValid())
+    SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "Incorrect connector template - connector variable is not found.");
 }
 
 void TemplateUtils::getWeightTemplateKeyElements(
@@ -33,8 +39,12 @@ void TemplateUtils::getWeightTemplateKeyElements(
 {
   elements.measuredObjectVariable =
       IteratorUtils::getAnyByOutRelation(&context, connectorWeightTemplateAddr, Keynodes::GetRrelIndex(1));
+  if(!elements.measuredObjectVariable.IsValid())
+    SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "Incorrect connector weight template - measured object is not found.");
   elements.numberVariable =
       IteratorUtils::getAnyByOutRelation(&context, connectorWeightTemplateAddr, Keynodes::GetRrelIndex(2));
+  if(!elements.numberVariable.IsValid())
+    SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "Incorrect connector weight template - number variable is not found.");
 }
 
 }  // namespace utils

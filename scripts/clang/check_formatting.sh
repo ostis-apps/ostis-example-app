0f80a5d62ba34e65345186a879344a9dc3f1c264
NikiforovSergei <nikiforov.sergei.al@gmail.com>
2025-01-20 16:12:54 +0300

    fix(scripts): typo in code formatting check script

--- scripts/clang/check_formatting.sh
+++ scripts/clang/check_formatting.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 if [ -z "${BUILD_PATH}" ];
 then
-  source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/.."/set_vars.sh
+  source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/../set_vars.sh"
 fi
 
 cmake -B "${BUILD_PATH}" -DSC_CLANG_FORMAT_CODE=ON
